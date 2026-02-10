38aa013c4830e50e03171653043f7a056e5d5b86
Veirt <veirt@duck.com>
2026-02-11 01:26:08 +0800

    feat(airplanes): add trails and colored airplane art

--- src/animation/airplanes.rs
+++ src/animation/airplanes.rs
@@ -7,6 +7,7 @@ struct Airplane {
     x: f32,
     y: f32,
     speed: f32,
+    trail_positions: Vec<f32>,
 }
 
 pub struct AirplaneSystem {
@@ -32,6 +33,11 @@ impl AirplaneSystem {
 
         for plane in &mut self.planes {
             plane.x += plane.speed;
+
+            plane.trail_positions.insert(0, plane.x);
+            if plane.trail_positions.len() > 8 {
+                plane.trail_positions.truncate(8);
+            }
         }
 
         self.planes.retain(|p| p.x < terminal_width as f32);
@@ -47,7 +53,12 @@ impl AirplaneSystem {
         let y = (rand::random::<u16>() % (self.terminal_height / 4)) as f32;
         let speed = 0.3 + (rand::random::<f32>() * 0.2);
 
-        self.planes.push(Airplane { x: 0.0, y, speed });
+        self.planes.push(Airplane {
+            x: 0.0,
+            y,
+            speed,
+            trail_positions: Vec::new(),
+        });
     }
 
     pub fn render(&self, renderer: &mut TerminalRenderer) -> io::Result<()> {
@@ -65,6 +76,21 @@ impl AirplaneSystem {
             let x = plane.x as u16;
             let y = plane.y as u16;
 
+            for (i, &trail_x) in plane.trail_positions.iter().enumerate() {
+                let trail_render_x = trail_x as u16;
+                let trail_y = y + 3;
+
+                if trail_render_x < self.terminal_width && trail_y < self.terminal_height {
+                    let (ch, color) = match i {
+                        0..=1 => ('.', Color::White),
+                        2..=3 => ('.', Color::Grey),
+                        4..=5 => ('.', Color::DarkGrey),
+                        _ => ('·', Color::DarkGrey),
+                    };
+                    renderer.render_char(trail_render_x, trail_y, ch, color)?;
+                }
+            }
+
             for (line_offset, line) in airplane_art.iter().enumerate() {
                 let render_y = y + line_offset as u16;
                 if render_y >= self.terminal_height {
@@ -78,7 +104,18 @@ impl AirplaneSystem {
                     }
 
                     if ch != ' ' {
-                        renderer.render_char(render_x, render_y, ch, Color::White)?;
+                        let color = match ch {
+                            '"' => Color::Cyan,
+
+                            '\\' => Color::Blue,
+
+                            '_' => Color::DarkGrey,
+
+                            '~' => Color::Grey,
+
+                            _ => Color::White,
+                        };
+                        renderer.render_char(render_x, render_y, ch, color)?;
                     }
                 }
             }
